15c928c89254e66249c70e1df92540bda2c8009f
Anthony Libetti <anthony.libetti@fa-AnthonyLibetti.local>
2023-01-11 13:15:41 -0500

    Remove unwanted import

--- src/components/reactmapgl/MapBoxMap.tsx
+++ src/components/reactmapgl/MapBoxMap.tsx
@@ -1,17 +1,8 @@
 // Install map libre and create a base map, gzip and minify the project
 import React, { FunctionComponent } from 'react';
-import maplibregl from 'maplibre-gl';
 import Map from "react-map-gl"
 import 'maplibre-gl/dist/maplibre-gl.css';
 
-
-// Do we couple the container logic into the map itself and just pass props to change the style?
-// Alt -  the container is created in the component that consumes the map.
-// Right now we'll wrap it in a div to apply styling globally
-
-// Do we want to use a version with mapbox, do we have a license through collins that has enough seats
-// Ideally we can use the base map libre package, federate all the different features of our flight pages out and then
-// plug them in as we need them. I.E weather, planes, airports, fuel estimation,
 
 type MapProps = {
 	baseLayer: string,
