040c172d29ee6f4010afc924047bc3b07bdf0055
Anthony Libetti <anthony.libetti@fa-AnthonyLibetti.local>
2023-01-16 16:16:55 -0500

    Added react-leaflet no ssr

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,13 @@
 import React, {FunctionComponent, useState} from "react"
+import dynamic from "next/dynamic";
 import MapLibreMap from "../components/reactmapgl/MapLibreMap"
 import MapBoxMap from "../components/reactmapgl/MapBoxMap"
+import ReactLeafletMap from "../components/reactleaflet/ReactLeafletMap"
 import Header from "../components/layout/Header"
 import Content from "../components/layout/Content"
 import Footer from "../components/layout/Footer"
+import 'leaflet/dist/leaflet.css'
+
 
 const frameworks = ["React-Map-GL (Libre)","React-Map-GL (Mapbox)","React-Leaflet"]
 
@@ -11,12 +15,16 @@ const HomePage: FunctionComponent<{}> = () => {
 	const [tab,setTab] = useState<number>(0)
 	let component = <MapLibreMap baseLayer="Topographic"/>
 	switch (tab) {
+		case 0:
+			component = <MapLibreMap key="MapLibreMap" baseLayer="Topographic"/>
+			break;
 		case 1:
-			component = <MapBoxMap baseLayer="Topographic"/>
+			component = <MapBoxMap key="MapBoxMap" baseLayer="Topographic"/>
 			break;
 		case 2:
-			// component = <ReactLeafletMap baseLayer="Topographic" isFullscreen={false} />
+			component = <ReactLeafletMap key="LeafletNoSSR"/>
 			break;
+
 		default:
 			break;
 	}
