4cd4da80f0dd76a42c4f7041e483adfec3a62ec3
Anthony Libetti <anthony.libetti@fa-AnthonyLibetti.local>
2023-01-17 14:50:51 -0500

    add 100vh and 100vw to leaflet

--- src/components/reactleaflet/DynamicMap.tsx
+++ src/components/reactleaflet/DynamicMap.tsx
@@ -5,7 +5,8 @@ import 'leaflet/dist/leaflet.css'
 
 const DynamicMap: FunctionComponent<{}> = () => {
 	return (
-		<MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+		<MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false} style={{width: '100vw', height: '100vh'}}
+		>
 			<TileLayer
        			attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         		url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
