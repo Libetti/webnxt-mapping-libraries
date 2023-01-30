ef1e4057ed66d43e2f926fa85cdee7c06b7a153c
Anthony Libetti <anthony.libetti@fa-AnthonyLibetti.local>
2023-01-30 13:36:03 -0500

    added aeris weather layers to maplibre

--- src/components/reactmapgl/MapLibreMap.tsx
+++ src/components/reactmapgl/MapLibreMap.tsx
@@ -1,86 +1,125 @@
 // Install map libre and create a base map, gzip and minify the project
-import React, {useState, useMemo, FunctionComponent, useRef} from 'react';
-import maplibregl from 'maplibre-gl';
-import Map, {Popup, Layer, Source, MapLayerMouseEvent, MapRef} from "react-map-gl"
-import 'maplibre-gl/dist/maplibre-gl.css';
-import {airportLayerProps, airportGeoJson} from '../../utils/utils'
-import * as mapsgl from '@aerisweather/mapsgl';
-import '@aerisweather/mapsgl/dist/mapsgl.css';
-// import AirportIcon from "../../../public/airport.png"
+import React, {
+    useState,
+    useMemo,
+    FunctionComponent,
+    useRef,
+} from "react";
+import maplibregl from "maplibre-gl";
+import Map, {
+    Popup,
+    Layer,
+    Source,
+    MapLayerMouseEvent,
+    MapRef,
+} from "react-map-gl";
+import "maplibre-gl/dist/maplibre-gl.css";
+import { airportLayerProps, airportGeoJson } from "../../utils/utils";
 
 type MapProps = {
-	baseLayer: string,
-}
+    baseLayer: string;
+};
 
 const MapLibreMap: FunctionComponent<MapProps> = ({ baseLayer }) => {
+    const [selectedAirport, setSelectedAirport] = useState<{
+        longitude: number;
+        latitude: number;
+        name: string;
+        type: string;
+    } | null>(null);
 
-	const weatherLayer:any = {
-		id: 'WeatherLayer',
-		type: 'raster',
-		  paint: {
-		  'raster-opacity': 0.5
-		},
-		'background-opacity': 0.9,
-		minzoom: 0,
-		maxzoom: 22,
-	};
+    const mapRef = useRef<MapRef>();
 
-	const [selectedAirport,setSelectedAirport] = useState<{longitude:number,latitude:number, name: string, type: string} | null>(null)
-	const mapRef = useRef<MapRef>()
-	const onMapLoad = () => {
-		mapRef.current.loadImage(
-			"/airport.png",
-			(error, image) => {
-				if (error) throw error;
-				 
-				// Add the image to the map style.
-				mapRef.current.addImage('airportImage', image);
-				 
-			}
-		)
-		console.log(window.navigator)
+    const onMapLoad = () => {
+        mapRef.current.loadImage("/airport.png", (error, image) => {
+            if (error) throw error;
 
-		// const account = new mapsgl.Account('key', 'secret');3
-		// const controller = new mapsgl.MapboxMapController(mapRef.current.getMap(), { account });
-		// controller.on('load', () => {
-		// 	// do stuff, like add weather layers
-		// 	controller.addWeatherLayer('accum-precip-1hr');
-		// });
-	}
-	const renderMarkers = useMemo<any>(() => airportGeoJson(),[])	
+            // Add the image to the map style.
+            mapRef.current.addImage("airportImage", image);
+        });
+    };
 
-	const onMapClick = (event: MapLayerMouseEvent) => {
-		const selectedFeatures = mapRef.current.queryRenderedFeatures(event.point, {
-			layers: ['airport-markers']
-		});
-		if(selectedFeatures.length){
-			setSelectedAirport({longitude: event.lngLat.lng, latitude:event.lngLat.lat , name: selectedFeatures[0].properties.name, type:selectedFeatures[0].properties.name ,})
-		}
-	}
+    const weatherSourceProps: any = {
+        id: "OpenWeatherSource",
+        type: "raster",
+        tiles: [
+            `https://maps.openweathermap.org/maps/2.0/radar/6/13/23?&appid=${process.env.OPENWEATHER_KEY}&tm=1600781400`,
+        ],
+        tileSize: 256,
+    };
 
-	return <>
-		<Map
-			mapLib={maplibregl}
-			initialViewState={{
-				longitude: -122.4,
-				latitude: 37.8,
-				zoom: 4
-			}}
-			style={{width: '100vw', height: '100vh'}}
-			mapStyle="https://basemaps.cartocdn.com/gl/positron-gl-style/style.json"
-			interactiveLayerIds={["airport-markers"]}
-			onLoad={onMapLoad}
-			onClick={onMapClick}
-			ref={mapRef}
-		>			
-		{selectedAirport && (
-			<Popup longitude={selectedAirport.longitude} latitude={selectedAirport.latitude} onClose={() => setSelectedAirport(null)}><table><thead><tr><th>Name</th></tr></thead><tbody><tr><td>{selectedAirport.name}</td></tr></tbody></table></Popup>
-		)}
-		<Source key="AirportSource" type="geojson" data={renderMarkers}>
-          <Layer {...airportLayerProps()} />
-        </Source>
-		</Map>
-	</>
-}
+    const weatherLayerProps: any = {
+        id: "OpenWeatherSource",
+        type: "raster",
+        source: "OpenWeatherSource",
+        minzoom: 4,
+        maxzoom: 10,
+    };
 
-export default MapLibreMap
+    const renderMarkers = useMemo<any>(() => airportGeoJson(), []);
+
+    const onMapClick = (event: MapLayerMouseEvent) => {
+        const selectedFeatures = mapRef.current.queryRenderedFeatures(
+            event.point,
+            {
+                layers: ["airport-markers"],
+            }
+        );
+        if (selectedFeatures.length) {
+            setSelectedAirport({
+                longitude: event.lngLat.lng,
+                latitude: event.lngLat.lat,
+                name: selectedFeatures[0].properties.name,
+                type: selectedFeatures[0].properties.name,
+            });
+        }
+    };
+
+    return (
+        <>
+            <Map
+                mapLib={maplibregl}
+                initialViewState={{
+                    longitude: -122.4,
+                    latitude: 37.8,
+                    zoom: 4,
+                }}
+                style={{ width: "100vw", height: "100vh" }}
+                mapStyle="https://basemaps.cartocdn.com/gl/positron-gl-style/style.json"
+                interactiveLayerIds={["airport-markers"]}
+                onLoad={onMapLoad}
+                onClick={onMapClick}
+                ref={mapRef}
+            >
+                {selectedAirport && (
+                    <Popup
+                        longitude={selectedAirport.longitude}
+                        latitude={selectedAirport.latitude}
+                        onClose={() => setSelectedAirport(null)}
+                    >
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th>Name</th>w
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>{selectedAirport.name}</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </Popup>
+                )}
+                <Source key="AirportSource" type="geojson" data={renderMarkers}>
+                    <Layer {...airportLayerProps()} />
+                </Source>
+                {/* <Source key="WeatherSource" {...weatherSourceProps}>
+                    <Layer {...weatherLayerProps} />
+                </Source> */}
+            </Map>
+        </>
+    );
+};
+
+export default MapLibreMap;
